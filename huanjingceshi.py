f9b3e6a60411613fc4e5547947f457a7554da3ad
bujiersu <47764263+bujiersu@users.noreply.github.com>
2023-03-24 02:14:10 +0800

    Update huanjingceshi.py

--- huanjingceshi.py
+++ huanjingceshi.py
@@ -5,6 +5,8 @@ print("CUDA数量 = ", torch.cuda.device_count()) # 查看可用的CUDA数量
 print("CUDA版本 = ", torch.version.cuda) # 查看CUDA的版本号
 print("cuDNN版本 = ", torch.backends.cudnn.version())
 
+print("torch版本 = ",torch.__version__)
+
 print("GPU = ", torch.cuda.is_available()) # 测试GPU是否可用
 print("GPU数量 = ", torch.cuda.device_count()) # 测试GPU数量
 print("GPU信息 = ", torch.cuda.get_device_properties(0)) # 测试第一块显卡的具体信息
